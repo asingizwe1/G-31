3cee7ca1c6455d7af54029647521697744f7a755
Sharif185 <sharifssentaayi1@gmail.com>
2024-07-26 09:56:40 +0300

    Add files via upload
    migration for the schools data

---
+++ 2024_07_12_011119_create_schools_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->id();
+            $table->string('reg_no');
+            $table->string('name');
+            $table->string('district');
+            $table->string('school_representative_name');
+            $table->string('school_representative_email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+};
